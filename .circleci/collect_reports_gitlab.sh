57b496ee2751d283a6bf407243128b8a501c7ac5
Vyacheslav Voronenko <git@voronenko.info>
2019-06-14 16:38:54 +0200

    improve(pipeline) collecting tests results

---
+++ .circleci/collect_reports_gitlab.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Save all important reports into (project-root)/reports
+# This folder will be saved by gitlab and available after test runs.
+
+set -e
+
+REPORTS_DIR=./reports
+mkdir -p $REPORTS_DIR >/dev/null 2>&1
+
+cp /tmp/hs_err_pid*.log $REPORTS_DIR || true
+
+function save_reports () {
+    project_to_save=$1
+    echo "saving reports for $project_to_save"
+
+    report_path=$REPORTS_DIR/$project_to_save
+    mkdir -p $report_path
+    cp -r $CI_PROJECT_DIR/$project_to_save/build/reports/* $report_path/
+}
+
+shopt -s globstar
+
+for report_path in $CI_PROJECT_DIR/**/build/reports; do
+    echo $report_path
+    report_path=${report_path//workspace\//}
+    report_path=${report_path//\/build\/reports/}
+    save_reports $report_path
+done
---
+++ .circleci/collect_results_gitlab.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Save all important reports and artifacts into (project-root)/results
+# This folder will be saved by gitlab and available after test runs.
+
+set -e
+
+TEST_RESULTS_DIR=./results
+mkdir -p $TEST_RESULTS_DIR >/dev/null 2>&1
+
+echo "saving test results"
+mkdir -p $TEST_RESULTS_DIR/results
+find $CI_PROJECT_DIR/**/build/test-results -name \*.xml -exec cp {} $TEST_RESULTS_DIR \;
